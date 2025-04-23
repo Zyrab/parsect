87e44a9df8acdaad57ec2e4585d8e0e3aed26586
zyrab <zurakrua@gmail.com>
2025-04-24 00:16:12 +0200

    feat: added favicon/ theme ow is set from system

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,6 +1,13 @@
 import { Domo } from "@zyrab/domo";
 
 export default function createHeader() {
+  const prefersDarkScheme = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  if (prefersDarkScheme) {
+    document.body.classList.add("dark");
+  }
   function handleThemeChange(e) {
     e.preventDefault();
     document.body.classList.toggle("dark");
@@ -14,7 +21,7 @@ export default function createHeader() {
         .css({ cursor: "pointer" })
         .cls("material-symbols-outlined")
         .txt("clear_day")
-        .on({ click: handleThemeChange })
+        .on("click", handleThemeChange)
         .build(),
     ]);
 
