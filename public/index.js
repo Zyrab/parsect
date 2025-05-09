78b09f94caee2284e137b21864738e9fc93b776f
zyrab <zurakrua@gmail.com>
2025-05-10 01:30:04 +0200

    ref: srr im forgot thefooter

--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
 const app = document.getElementById("app");
 
 import createHeader from "../src/app/layout/header.js";
-import createFooter from "../src/app/layout/footer.js";
 import createAppView from "../src/app/view/appView.js";
 
 function initApp() {
   app.appendChild(createHeader());
   app.appendChild(createAppView());
-  app.appendChild(createFooter());
 }
 
 document.addEventListener("DOMContentLoaded", () => initApp());
